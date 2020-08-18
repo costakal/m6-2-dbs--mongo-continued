389c4175f97f669d16fc885ca9cdb3a2d55c172f
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-08-18 14:51:18 -0400

    Exercise 1

--- server/routes.js
+++ server/routes.js
@@ -1,63 +1,71 @@
 const router = require("express").Router();
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
 const NUM_OF_ROWS = 8;
 const SEATS_PER_ROW = 12;
 
-const delay = (time) => {
-  return new Promise((res, rej) => {
-    setTimeout(res, time);
-  });
-};
-
 // Code that is generating the seats.
 // ----------------------------------
 const seats = {};
-const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
-for (let r = 0; r < row.length; r++) {
-  for (let s = 1; s < 13; s++) {
-    seats[`${row[r]}-${s}`] = {
-      price: 225,
-      isBooked: false,
-    };
+
+const generateSeats = () => {
+  const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
+  for (let r = 0; r < row.length; r++) {
+    for (let s = 1; s < 13; s++) {
+      seats[`${row[r]}-${s}`] = {
+        _id: `${row[r]}-${s}`,
+        price: 225,
+        isBooked: false,
+      };
+    }
   }
-}
+};
 // ----------------------------------
 //////// HELPERS
 const getRowName = (rowIndex) => {
   return String.fromCharCode(65 + rowIndex);
 };
 
-const randomlyBookSeats = (num) => {
-  const bookedSeats = {};
-
-  while (num > 0) {
-    const row = Math.floor(Math.random() * NUM_OF_ROWS);
-    const seat = Math.floor(Math.random() * SEATS_PER_ROW);
-
-    const seatId = `${getRowName(row)}-${seat + 1}`;
-
-    bookedSeats[seatId] = true;
-
-    num--;
-  }
-
-  return bookedSeats;
-};
-
 let state;
 
 router.get("/api/seat-availability", async (req, res) => {
-  if (!state) {
-    state = {
-      bookedSeats: randomlyBookSeats(30),
-    };
-  }
+  const databaseSetup = async () => {
+    const client = await MongoClient(MONGO_URI, options);
+    await client.connect();
+    console.log("connected");
 
-  await delay(Math.random() * 3000);
+    const db = client.db("flights");
+    const dbSeats = db.collection("seats");
+
+    const dbSeatsArray = await dbSeats.find().toArray();
+
+    if (dbSeatsArray.length === 0) {
+      console.log("initial setup");
+      generateSeats();
+      await dbSeats.insertMany(Object.values(seats));
+    } else {
+      dbSeatsArray.map((seat) => {
+        seats[seat._id] = seat;
+      });
+    }
+
+    client.close();
+    console.log("closing");
+  };
+
+  await databaseSetup();
+  console.log("List of Seats: ", seats);
 
   return res.json({
     seats: seats,
-    bookedSeats: state.bookedSeats,
     numOfRows: 8,
     seatsPerRow: 12,
   });
@@ -68,15 +76,7 @@ let lastBookingAttemptSucceeded = false;
 router.post("/api/book-seat", async (req, res) => {
   const { seatId, creditCard, expiration } = req.body;
 
-  if (!state) {
-    state = {
-      bookedSeats: randomlyBookSeats(30),
-    };
-  }
-
-  await delay(Math.random() * 3000);
-
-  const isAlreadyBooked = !!state.bookedSeats[seatId];
+  const isAlreadyBooked = state.seats[seatId].isBooked;
   if (isAlreadyBooked) {
     return res.status(400).json({
       message: "This seat has already been booked!",
@@ -100,6 +100,7 @@ router.post("/api/book-seat", async (req, res) => {
 
   lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
 
+  // to do connect to mongo to change specific seat from false to true
   state.bookedSeats[seatId] = true;
 
   return res.status(200).json({
